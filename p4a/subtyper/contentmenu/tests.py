abd7e60c87852119f3d6dd0ce2ed569b8446064e
Ross Patterson <me@rpatterson.net>
2008-03-26 18:19:23 +0000

    Only run the contentmenu tests under Plone 2.5 / Zope 2.9.
    git-svn-id: http://svn.plone.org/svn/collective/p4a/p4a.subtyper/trunk@91675 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- p4a/subtyper/contentmenu/tests.py
+++ p4a/subtyper/contentmenu/tests.py
@@ -9,11 +9,16 @@ PloneTestCase.setupPloneSite()
 
 def test_suite():
     flags = doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS
-    fsuite = unittest.TestSuite((
-        FunctionalDocFileSuite('browser.txt',
-                               package='p4a.subtyper.contentmenu',
-                               optionflags=flags),
-        ))
-    fsuite.layer = Products.PloneTestCase.layer.PloneSite
+    fsuite = unittest.TestSuite(())
+    try:
+        # this is to make sure the tests only run in a plone 2.5 / zope 2.9 environment
+        import zope.app.annotation.tests
+        fsuite.append(FunctionalDocFileSuite('browser.txt',
+                                             package='p4a.subtyper.contentmenu',
+                                             optionflags=flags),)
+        fsuite.layer = Products.PloneTestCase.layer.PloneSite
+    except ImportError, err:
+        # this is ok
+        pass
 
     return fsuite
