c69af70a216f75d7a39761823e315a5d1a610fc8
Rocky Burt <rockyburt@gmail.com>
2008-10-07 15:50:12 +0000

    Made Plone4ArtistsSubtyper setup optional.
    git-svn-id: http://svn.plone.org/svn/collective/p4a/p4a.subtyper/trunk@91948 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- p4a/subtyper/contentmenu/tests.py
+++ p4a/subtyper/contentmenu/tests.py
@@ -4,14 +4,22 @@ from Testing.ZopeTestCase import FunctionalDocFileSuite
 import Products.PloneTestCase.layer
 
 from Products.PloneTestCase import PloneTestCase
-PloneTestCase.installProduct('Plone4ArtistsSubtyper')
+try:
+    import Products.Plone4ArtistsSubtyper
+    PloneTestCase.installProduct('Plone4ArtistsSubtyper')
+except ImportError, err:
+    # this is ok
+    pass
+
 PloneTestCase.setupPloneSite()
+
 
 def test_suite():
     flags = doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS
     fsuite = unittest.TestSuite(())
     try:
-        # this is to make sure the tests only run in a plone 2.5 / zope 2.9 environment
+        # this is to make sure the tests only run
+        # in a plone 2.5 / zope 2.9 environment
         import zope.app.annotation.tests
         fsuite.append(FunctionalDocFileSuite('browser.txt',
                                              package='p4a.subtyper.contentmenu',
